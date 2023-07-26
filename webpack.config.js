524776f29cd873a4fc5e9f0577ee5dd1c7736a75
pavelnk <pav_nk@vk.com>
2023-07-26 18:22:14 +0300

    fix: edit configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,15 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const FileManagerPlugin = require('filemanager-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 module.exports = {
-	entry: path.join(__dirname, 'src', 'index.js'),
+	entry: path.join(__dirname, 'src', 'index.tsx'),
 	output: {
     path: path.join(__dirname, 'dist'),
     filename: 'index.[contenthash:4].js',
-		assetModuleFilename: 'images/[name].[contenthash:4][ext]',
+    clean: true,
   },
 	module: {
     rules: [
-			{
-        test: /\.m?js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
 			{
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -26,22 +17,7 @@ module.exports = {
       },
 			{
         test: /\.styl$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader', 'stylus-loader'],
-      },
-			{
-        test: /\.(png|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.svg$/i,
-        type: 'asset/resource',
-        generator: {
-          filename: 'icons/[name].[contenthash:4][ext]',
-        },
-      },
-			{
-        test: /\.(woff2?|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        use: ['style-loader', 'css-loader', 'stylus-loader'],
       },
     ],
   },
@@ -49,18 +25,11 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'public', 'index.html'),
       filename: 'index.html',
-    }),
-    new FileManagerPlugin({
-      events: {
-        onStart: {
-          delete: ['dist'],
-        },
-      },
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].[contenthash:4].css',
-    }),
+    })
   ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
 	devServer: {
     watchFiles: path.join(__dirname, 'src'),
     port: 9000,
