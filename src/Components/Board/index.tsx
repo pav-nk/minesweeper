8cf7f281e9c1957f0da51c1d4f10f52186156a82
pavelnk <pav_nk@vk.com>
2023-09-22 08:46:30 +0300

    fix: fixing the board component

--- src/Components/Board/index.tsx
+++ src/Components/Board/index.tsx
@@ -4,64 +4,88 @@ const ROWS_COUNT = 10;
 const CELLS_COUNT = 10;
 
 const getRandomNum = (num: number): number => {
-    // TODO: доработать getRandomNum
     return Math.floor(Math.random() * num);
 };
+
+const generateCoord = (rowsCount: number, cellsCount: number): [number, number] => [
+    getRandomNum(rowsCount),
+    getRandomNum(cellsCount),
+];
 
 const getRandomCoord = (
     rowsCount: number,
     cellsCount: number,
     board: number[][],
 ): [number, number] => {
-    // TODO: проверка существует ли на поле -1 если нет сгенерировать еще раз
-    const newRandomCoord: [number, number] = [
-        getRandomNum(rowsCount),
-        getRandomNum(cellsCount),
-    ];
-    const [newRowsCoord, newCellsCoord] = newRandomCoord;
-    const boardRow = board[newRowsCoord];
-    if (boardRow) {
-        if (boardRow[newCellsCoord] === -1)
-            return getRandomCoord(ROWS_COUNT, CELLS_COUNT, board);
+    let [row, cell] = generateCoord(rowsCount, cellsCount);
+
+    while (board[row]?.[cell] === -1) {
+        [row, cell] = generateCoord(rowsCount, cellsCount);
     }
-    return newRandomCoord;
+
+    return [row, cell];
 };
 
 // TODO: написать функцию которая посчитает на каждой ячейке количество мин вокруг нее.
 
-const getBoardWithNumValues = (board: number[][]): number[][] => {
-    const result: number[][] = [];
-    for (let row = 0; row < board.length; row += 1) {
-        const currentRow = board[row];
-        if (currentRow) {
-            for (let col = 0; col < currentRow.length; col += 1) {
-                let currentValue = currentRow[col];
+const STEPS_DICT = [
+    [-1, -1],
+    [-1, 0],
+    [-1  1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1  1],
+]
 
-                if (currentValue === -1) {
-                    continue;
-                }
+// 1 замутировать масссив board
+// 2 пройтись по каждой клетке 
+// 
 
-                let minesCount = 0;
+// Можно расстановку мин и калькуляцию сделать гораздо проще и дешевле:
+// 1) На поле расставляем нули
+// 2) Запускаем цикл с расстановкой бомб
+// 3) На каждом шаге цикла устанавливаем бомбу и сразу же вокруг нее все инкрементируем на 1. Только не забудь сделать проверку, если сосед -1, то инкрементировать его не надо. 
+// Вуаля, йопт
 
-                for (const stepRow of [-1, 0, 1]) {
-                    for (const stepCol of [-1, 0, 1]) {
-                        const stepRowCurrent = board[row + stepRow];
-                        if (stepRowCurrent) {
-                            const colValue = stepRowCurrent[col + stepCol];
-                            if (colValue === undefined) continue;
-                            if (colValue === -1) {
-                                minesCount += 1;
-                            }
-                        }
-                    }
-                }
+// const getBoardWithNumValues = (board: number[][]): number[][] => {
+//     const result: number[][] = [];
+//     for (let row = 0; row < board.length; row += 1) {
+//         const currentRow = board[row];
+//         if (currentRow) {
+//             for (let col = 0; col < currentRow.length; col += 1) {
+//                 let currentValue = currentRow[col];
 
-                currentRow[col] = minesCount;
-            }
-            result.push(currentRow);
-        }
-    }
-    return result;
+//                 if (currentValue === -1) {
+//                     continue;
+//                 }
+
+//                 let minesCount = 0;
+
+//                 for (const stepRow of [-1, 0, 1]) {
+//                     for (const stepCol of [-1, 0, 1]) {
+//                         const stepRowCurrent = board[row + stepRow];
+//                         if (stepRowCurrent) {
+//                             const colValue = stepRowCurrent[col + stepCol];
+//                             if (colValue === undefined) continue;
+//                             if (colValue === -1) {
+//                                 minesCount += 1;
+//                             }
+//                         }
+//                     }
+//                 }
+
+//                 currentRow[col] = minesCount;
+//             }
+//             result.push(currentRow);
+//         }
+//     }
+//     return result;
+// };
+
+const addValuesAroundMine = (row: number, cell: number, board: number[][]) => {
+
 };
 
 export function Board() {
@@ -78,16 +102,15 @@ export function Board() {
         for (let i = 0; i < minesCount; i += 1) {
             const [row, cell] = getRandomCoord(ROWS_COUNT, CELLS_COUNT, boardNext);
             const boardNextRow = boardNext[row];
-            if (boardNextRow) boardNextRow[cell] = -1;
+            if (boardNextRow) {
+                boardNextRow[cell] = -1;
+                addValuesAroundMine(row, cell, boardNext);
+            }
         }
 
         console.log(boardNext);
 
-        const boardWithNumValues: number[][] = getBoardWithNumValues(boardNext);
-
-        console.log(boardWithNumValues);
-
-        setBoard(boardWithNumValues);
+        setBoard(boardNext);
     }, []);
 
     console.log(board);
